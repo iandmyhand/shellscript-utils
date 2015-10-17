168bcf1d420c4bf4c39df8f47776e7c6290e8ab1
SeomGi, Han <iandmyhand@gmail.com>
2015-10-17 21:43:10 +0900

    Add a script to change Mac Address easily.

---
+++ changeMacAddress.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+MAC_MACBOOK=
+MAC_IPHONE=
+SUPPORTED_DEVICES=("iPhone" "Macbook")
+
+usage() { echo "Usage: $0 ['iPhone'|'Macbook']" 1>&2; echo ""; exit 1; }
+
+isIn () {
+    local e
+    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+    return 1
+}
+
+changeMacAddressTo() {
+    DEVICE=$1
+    case "$DEVICE" in
+        "iPhone") sudo ifconfig en0 ether $MAC_IPHONE
+        ;;
+        "Macbook") sudo ifconfig en0 ether $MAC_MACBOOK
+        ;;
+    esac
+    sudo ifconfig en0 down
+    sudo ifconfig en0 up
+}
+
+DEVICE=$1
+isIn $DEVICE "${SUPPORTED_DEVICES[@]}"
+existed=$?
+if [ $existed -eq 1 ]; then
+    usage
+else
+    changeMacAddressTo $DEVICE
+fi
+exit 0
